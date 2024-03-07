33a6c7728b48a9b0378faa3e5e7e068113581dc2
Otto Giron <ottog2486@gmail.com>
2024-03-07 23:59:27 +0100

    Use only init.lua

--- .vimrc
+++ .vimrc
@@ -1,190 +1,46 @@
-call plug#begin('~/.vim/bundle')
-call plug#end()
-set ttyfast
-
-set clipboard=unnamedplus
-
-
-" Mapping Configuration
-" This comes first, because we have mappings that depend on leader
-" With a map leader it's possible to do extra key combinations
-" i.e: <leader>w saves the current file
+" Set leader key
 let mapleader = ","
-" Start with basic settngs
-" Set Vim-specific options (use :help option-name to find out more about each)
+
+" Basic editing configurations
 set nocompatible              " Disable vi compatibility mode
 set backspace=indent,eol,start " Make backspace work over everything in insert mode
 set autoindent                " Enable auto-indent
 set smartindent               " Enable smart indent
-set tabstop=4                 " Set tab to 4 spaces
-set shiftwidth=4              " Set number of auto-indent spaces
-set expandtab                 " Use spaces instead of tabs
-set softtabstop=4             " Set number of spaces per Tab press
+set tabstop=4                 " Set tab width to 4 spaces
+set shiftwidth=4              " Set auto-indent width to 4 spaces
+set expandtab                 " Convert tabs to spaces
+set softtabstop=4             " Soft tab stops at 4 spaces
 set number                    " Show line numbers
 set relativenumber            " Show relative line numbers
 set wrap                      " Enable line wrap
-set scrolloff=5               " Keep at least 5 lines above/below
-set sidescrolloff=5           " Keep at least 5 lines left/right
+set scrolloff=5               " Minimal number of screen lines to keep above and below the cursor
+set sidescrolloff=5           " Minimal number of screen columns to keep to the left and right of the cursor
 set cursorline                " Highlight the current line
-set incsearch                 " Incremental search that shows partial matches
-set hlsearch                  " Highlight all search matches (use :noh to turn off temporarily)
-set ignorecase                " Case insensitive searching
-set smartcase                 " Case sensitive if upper case is included
+set incsearch                 " Incremental search
+set hlsearch                  " Highlight search results
+set ignorecase                " Case insensitive searching...
+set smartcase                 " ... unless query contains uppercase characters
 set wildmenu                  " Visual autocomplete for command menu
-set lazyredraw                " Don't redraw while executing macros (good performance config)
-set showmatch                 " Show matching brackets when text indicator is over them
-set matchtime=2               " Tenths of a second to show the matching parenthesis
+set showmatch                 " Show matching brackets
 
-" Visual options
-syntax on                     " Syntax highlighting
-colorscheme default           " Set colorscheme (substitute with your favorite colorscheme)
-set background=dark           " Tell vim what the background color looks like
-set t_Co=256                  " Use 256 colors - this is important for some colorschemes
+" Visual and performance improvements
+set lazyredraw                " Improve performance by not redrawing during macros
+set ttyfast                   " Speed up scrolling in Vim
 
-" Useful shortcuts
-nnoremap <C-S> :w<CR>         " Ctrl+S saves the file
-nnoremap <C-Q> :q<CR>         " Ctrl+Q quits the file
-inoremap <C-S> <Esc>:w<CR>i   " Ctrl+S saves the file from insert mode
-inoremap <C-Q> <Esc>:q<CR>i   " Ctrl+Q quits the file from insert mode
+" Clipboard to use system clipboard
+set clipboard=unnamedplus
 
-" Enable file type detection
-filetype on
-filetype indent on
-filetype plugin on
+" Mappings for saving and quitting
+nnoremap <C-s> :w<CR>          " Ctrl+S to save
+nnoremap <C-q> :q<CR>          " Ctrl+Q to quit
+inoremap <C-s> <Esc>:w<CR>i    " Ctrl+S to save in insert mode
+inoremap <C-q> <Esc>:q<CR>i    " Ctrl+Q to quit in insert mode
 
-" For plugins (if installed)
-"call plug#begin('~/.vim/plugged')
-" Plug 'tpope/vim-sensible'
-" Plug 'preservim/nerdtree'
-"call plug#end()
+" Enable filetype plugins
+filetype plugin indent on
 
-" More advanced mappings and commands can go here
+" Remove search highlight with leader+space
+nnoremap <leader><space> :nohlsearch<CR>
 
-" Remember, after making changes to your .vimrc, reload it with:
-" :source $MYVIMRC
-" Or just restart vim.
-
-" For more details on any of these settings, use the :help command, e.g.
-" :help scrolloff
-"end with basic settings
-
-" Some useful quickfix shortcuts for quickfix
-nnoremap <leader>a :cclose<CR>
-
-
-" Fast saving
-nnoremap <leader>w :w!<cr>
-nnoremap <silent> <leader>q :q!<CR>
-
-" Center the screen
-nnoremap <space> zz
-
-" Remove search highlight
- nnoremap <leader><space> :nohlsearch<CR>
-function! s:clear_highlight()
-  let @/ = ""
-  call go#guru#ClearSameIds()
-endfunction
-nnoremap <silent> <leader><space> :<C-u>call <SID>clear_highlight()<CR>
-
-" Better split switching
-map <C-j> <C-W>j
-map <C-k> <C-W>k
-map <C-h> <C-W>h
-map <C-l> <C-W>l
-
-" Kill job and close terminal window
-tnoremap <Leader>q <C-w><C-C><C-w>c<cr>
-
-" switch to normal mode with esc
-tnoremap <Esc> <C-W>N
-
-" mappings to move out from terminal to other views
-tnoremap <C-h> <C-w>h
-tnoremap <C-j> <C-w>j
-tnoremap <C-k> <C-w>k
-tnoremap <C-l> <C-w>l
-
-" Open terminal in vertical, horizontal and new tab
-
-nnoremap <leader>tv :vsplit<CR>:term<CR>
-nnoremap <leader>ts :split<CR>:term<CR>
-nnoremap <leader>tt :tabnew<CR>:term<CR>
-
-tnoremap <leader>tv <C-\><C-n>:vsplit<CR>:term<CR>
-tnoremap <leader>ts <C-\><C-n>:split<CR>:term<CR>
-
-
-
-" Visual linewise up and down by default (and use gj gk to go quicker)
-noremap <Up> gk
-noremap <Down> gj
-noremap j gj
-noremap k gk
-
-" Exit on j
-imap jj <Esc>
-
-" Source (reload configuration)
-nnoremap <F3> :source ~/.vimrc<CR>
-
-" Toggle Spell 
-nnoremap <F4> :setlocal spell! spell?<CR>
-
-" Search mappings: These will make it so that going to the next one in a
-" search will center on the line it's found in.
- nnoremap n nzzzv
- nnoremap N Nzzzv
-
-" Same when moving up and down
-noremap <C-d> <C-d>zz
-noremap <C-u> <C-u>zz
-
-" Do not show stupid q: window
-map q: :q
-
-
-
-" Plugins Configuration
-
-" ==================== NerdTree ====================
-" For toggling
-nnoremap <leader>n :NERDTreeFocus<CR>
-nnoremap <C-n> :NERDTree<CR>
-nnoremap <C-t> :NERDTreeToggle<CR>
-nnoremap <C-f> :NERDTreeFind<CR>
-
-" Start NERDTree, unless a file or session is specified, eg. vim -S session_file.vim.
-autocmd StdinReadPre * let s:std_in=1
-autocmd VimEnter * if argc() == 0 && !exists('s:std_in') && v:this_session == '' | NERDTree | endif
-
-" The Silver Searcher
-if executable('ag')
-  " Use ag over grep
-  set grepprg=ag\ --nogroup\ --nocolor
-
-  " Use ag in CtrlP for listing files. Lightning fast and respects .gitignore
-  let g:ctrlp_user_command = 'ag %s -l --nocolor -g ""'
-
-  " ag is fast enough that CtrlP doesn't need to cache
-  let g:ctrlp_use_caching = 0
-endif
-
-" CtrlP search config
-set wildignore+=*/tmp/*,*.so,*.swp,*.zip     " MacOSX/Linux
-set wildignore+=*\\tmp\\*,*.swp,*.zip,*.exe  " Windows
-set wildignore+=node_modules,*.git,*.hg,*.svn "
-
-" Java 
-set wildignore+=*.class,*.jar,*.war,*.ear,*.zip,*.tar.gz,*.tar.bz2,*.rar,*.pyc,*.o,*.a,*.so,*.dll,*.exe,*.obj,*.lib,*.ncb,*.sdf,*.suo,*.pdb,*.idb,*.ilk,*.aps,*.pch,*.res,*.tlb,*.tlh,*.bak,*.swp,*.bak,*.d,*.dSYM,*.class,*.jar,*.war
-
-" Java target dir
-set wildignore+=target,build,bin,classes,lib
-
-let g:ctrlp_custom_ignore = '\v[\/]\.(git|hg|svn)$'
-let g:ctrlp_custom_ignore = {
-  \ 'dir':  '\v[\/]\.(git|hg|svn|node_modules|vendor)$',
-  \ 'file': '\v\.(exe|so|dll)$',
-  \ }
-
-
+" Additional mappings and configurations can be handled in init.lua for Neovim specific setups
+" Remember to source this file if necessary or replicate configurations in init.lua for Neovim
--- init.lua
+++ init.lua
@@ -1,299 +1,134 @@
--------------------------------------------------------------------------------
--- These are example settings to use with nvim-metals and the nvim built-in
--- LSP. Be sure to thoroughly read the `:help nvim-metals` docs to get an
--- idea of what everything does. Again, these are meant to serve as an example.
--- If you just copy pasta them, they should work, but hopefully after time
--- goes on you'll cater them to your own liking especially since some of the stuff
--- in here is just an example, not what you probably want your setup to be.
---
--- Unfamiliar with Lua and Neovim?
---  - Check out `:help lua-guide`
---
--- The below configuration also makes use of the following plugins besides
--- nvim-metals, and therefore is a bit opinionated:
---
--- - https://github.com/hrsh7th/nvim-cmp
---   - hrsh7th/cmp-nvim-lsp for lsp completion sources
---   - hrsh7th/cmp-vsnip for snippet sources
---   - hrsh7th/vim-vsnip for snippet sources
---
--- - https://github.com/folke/lazy.nvim for package management
--- - https://github.com/mfussenegger/nvim-dap for debugging
--- - https://github.com/j-hui/fidget.nvim for progress notifications
--------------------------------------------------------------------------------
+-- Setting up global variables and utilities
 local map = vim.keymap.set
 local fn = vim.fn
+local cmd = vim.cmd
+local g = vim.g
 
--- Set runtimepath to include custom directories
-vim.opt.runtimepath:prepend("~/.vim")
+-- Set leader key to space
+g.mapleader = ' '
+g.maplocalleader = ' '
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- Lazy.nvim setup
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+  fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.runtimepath:append(lazypath)
 
--- Source your .vimrc file
- vim.cmd("source ~/.vimrc")
-
-----------------------------------
--- PLUGINS -----------------------
-----------------------------------
+-- Plugin setup
 require("lazy").setup({
-    {'rose-pine/neovim', name = 'rose-pine'},
-    {
-        'tpope/vim-sensible'
-    },
-    {
-        'tpope/vim-fugitive'
-    },
-    {
-        'mrcjkb/rustaceanvim',
-         version = '^4', -- Recommended
-         ft = { 'rust' },
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-        cmd = {
-        "TmuxNavigateLeft",
-        "TmuxNavigateDown",
-        "TmuxNavigateUp",
-        "TmuxNavigateRight",
-        "TmuxNavigatePrevious",
-    },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
+  {
+      'rose-pine/neovim',
+      name = 'rose-pine'
   },
   {
-    "kien/ctrlp.vim"
+      'mrcjkb/rustaceanvim',
+      version = '^4',
+      ft = { 'rust' }
   },
   {
-   "github/copilot.vim"
-  },
-  {"preservim/nerdtree"},
-  {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		{"nvim-lua/popup.nvim"},
-		{"nvim-lua/plenary.nvim"}
-	}
+      'christoomey/vim-tmux-navigator'
   },
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-vsnip" },
-      { "hrsh7th/vim-vsnip" }
-    },
-    opts = function()
-      local cmp = require("cmp")
-      local conf = {
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "vsnip" },
-        },
-        snippet = {
-          expand = function(args)
-            -- Comes from vsnip
-            fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          -- None of this made sense to me when first looking into this since there
-          -- is no vim docs, but you can't have select = true here _unless_ you are
-          -- also using the snippet stuff. So keep in mind that if you remove
-          -- snippets you need to remove this select
-          ["<CR>"] = cmp.mapping.confirm({ select = true })
-        })
+      'nvim-lua/popup.nvim'
+  },
+  {
+      'nvim-lua/plenary.nvim'
+  },
+  {
+      'nvim-telescope/telescope.nvim',
+      dependencies = {
+          'nvim-lua/popup.nvim',
+          'nvim-lua/plenary.nvim'
       }
-      return conf
-    end
   },
   {
-    "scalameta/nvim-metals",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-      {
-        "j-hui/fidget.nvim",
-        opts = {},
-      },
-      {
-        "mfussenegger/nvim-dap",
-        config = function(self, opts)
-          -- Debug settings if you're using nvim-dap
-          local dap = require("dap")
-
-          dap.configurations.scala = {
-            {
-              type = "scala",
-              request = "launch",
-              name = "RunOrTest",
-              metals = {
-                runType = "runOrTestFile",
-                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-              },
-            },
-            {
-              type = "scala",
-              request = "launch",
-              name = "Test Target",
-              metals = {
-                runType = "testTarget",
-              },
-            },
-          }
-        end
-      },
-    },
-    ft = { "scala", "sbt", "java" },
-    opts = function()
-      local metals_config = require("metals").bare_config()
-
-      -- Example of settings
-      metals_config.settings = {
-        showImplicitArguments = true,
-        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+      'hrsh7th/nvim-cmp',
+      event = "InsertEnter",
+      dependencies = {
+          'hrsh7th/cmp-nvim-lsp',
+          'hrsh7th/cmp-vsnip',
+          'hrsh7th/vim-vsnip'
       }
-
-      -- *READ THIS*
-      -- I *highly* recommend setting statusBarProvider to either "off" or "on"
-      --
-      -- "off" will enable LSP progress notifications by Metals and you'll need
-      -- to ensure you have a plugin like fidget.nvim installed to handle them.
-      --
-      -- "on" will enable the custom Metals status extension and you *have* to have
-      -- a have settings to capture this in your statusline or else you'll not see
-      -- any messages from metals. There is more info in the help docs about this
-      metals_config.init_options.statusBarProvider = "off"
-
-      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
-      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      metals_config.on_attach = function(client, bufnr)
-        require("metals").setup_dap()
---	require("telescope").extensions.metals.commands()
-
-        -- LSP mappings
-        map("n", "gD", vim.lsp.buf.definition)
-        map("n", "K", vim.lsp.buf.hover)
-        map("n", "gi", vim.lsp.buf.implementation)
-        map("n", "gr", vim.lsp.buf.references)
-        map("n", "gds", vim.lsp.buf.document_symbol)
-        map("n", "gws", vim.lsp.buf.workspace_symbol)
-        map("n", "<leader>cl", vim.lsp.codelens.run)
-        map("n", "<leader>sh", vim.lsp.buf.signature_help)
-        map("n", "<leader>rn", vim.lsp.buf.rename)
-        map("n", "<leader>f", vim.lsp.buf.format)
-        map("n", "<leader>ca", vim.lsp.buf.code_action)
-
-        map("n", "<leader>ws", function()
-          require("metals").hover_worksheet()
-        end)
-
-        -- all workspace diagnostics
-        map("n", "<leader>aa", vim.diagnostic.setqflist)
-
-        -- all workspace errors
-        map("n", "<leader>ae", function()
-          vim.diagnostic.setqflist({ severity = "E" })
-        end)
-
-        -- all workspace warnings
-        map("n", "<leader>aw", function()
-          vim.diagnostic.setqflist({ severity = "W" })
-        end)
-
-        -- buffer diagnostics only
-        map("n", "<leader>d", vim.diagnostic.setloclist)
-
-        map("n", "[c", function()
-          vim.diagnostic.goto_prev({ wrap = false })
-        end)
-
-        map("n", "]c", function()
-          vim.diagnostic.goto_next({ wrap = false })
-        end)
-
-        -- Example mappings for usage with nvim-dap. If you don't use that, you can
-        -- skip these
-        map("n", "<leader>dc", function()
-          require("dap").continue()
-        end)
-
-        map("n", "<leader>dr", function()
-          require("dap").repl.toggle()
-        end)
-
-        map("n", "<leader>dK", function()
-          require("dap.ui.widgets").hover()
-        end)
-
-        map("n", "<leader>dt", function()
-          require("dap").toggle_breakpoint()
-        end)
-
-        map("n", "<leader>dso", function()
-          require("dap").step_over()
-        end)
-
-        map("n", "<leader>dsi", function()
-          require("dap").step_into()
-        end)
-
-        map("n", "<leader>dl", function()
-          require("dap").run_last()
-        end)
-
-	-- Telescope
-	map("n", "<leader>tm", function()
-		require("telescope").extensions.metals.commands()
-	end)
-      end
-
-      return metals_config
-    end,
-    config = function(self, metals_config)
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = self.ft,
-        callback = function()
-          require("metals").initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-      })
-    end
-
-  }
+  },
+  {
+      'kyazdani42/nvim-tree.lua',
+      dependencies = {
+          'kyazdani42/nvim-web-devicons' -- for file icons
+      }
+  },
+  {
+      'neovim/nvim-lspconfig' -- LSP Configuration
+  },
+  {
+      'williamboman/nvim-lsp-installer' -- Simplified LSP installer
+  },
+  {
+      'mfussenegger/nvim-dap' -- Debugging (DAP)
+  },
+  {
+      'lewis6991/gitsigns.nvim' -- Git integration
+  },
 })
 
-----------------------------------
--- OPTIONS -----------------------
-----------------------------------
--- global
-vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+-- Nvim-tree setup
+require'nvim-tree'.setup {}
 
+-- Telescope setup
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = require('telescope.actions').close
+      },
+    },
+  },
+}
 
--- mappings
+-- Gitsigns setup
+require('gitsigns').setup {}
 
--- Telescope
-map("n", "<leader>tf", function()
-	require("telescope.builtin").find_files()
-end)
+-- LSP settings (example for Rust, extend as needed)
+require('nvim-lsp-installer').setup {}
+require'lspconfig'.rust_analyzer.setup {}
 
--- Custom
-vim.api.nvim_set_keymap('n', '<D-[>', ':bp<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<D-]>', ':bn<CR>', { noremap = true, silent = true })
+-- Nvim-cmp setup for autocompletion
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = true })
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+  })
+})
 
+-- Custom keybindings
+map("n", "<leader>ff", ":Telescope find_files<cr>", {silent = true})
+map("n", "<leader>fg", ":Telescope live_grep<cr>", {silent = true})
+map("n", "<leader>fb", ":Telescope buffers<cr>", {silent = true})
+map("n", "<leader>fh", ":Telescope help_tags<cr>", {silent = true})
+map("n", "<leader>n", ":NvimTreeToggle<CR>", {silent = true})
+map("n", "<leader>f", ":NvimTreeFindFile<CR>", {silent = true})
+
+-- Configure <leader> keybindings for common LSP functions
+map("n", "<leader>ld", ":lua vim.lsp.buf.definition()<CR>", {silent = true})
+map("n", "<leader>lh", ":lua vim.lsp.buf.hover()<CR>", {silent = true})
+map("n", "<leader>lr", ":lua vim.lsp.buf.rename()<CR>", {silent = true})
+map("n", "<leader>la", ":lua vim.lsp.buf.code_action()<CR>", {silent = true})
+
+-- Colorscheme
+cmd("colorscheme rose-pine")
