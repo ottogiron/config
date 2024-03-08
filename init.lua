e4d7edc938806aa535e85f2ccbcd87a18eef9272
Otto Giron <ottog2486@gmail.com>
2024-03-08 03:19:36 +0100

    Fix dap

--- init.lua
+++ init.lua
@@ -153,6 +153,28 @@ metals_config.on_attach = function(client, bufnr)
   map("n", "<leader>dso", function() require("dap").step_over() end)
   map("n", "<leader>dsi", function() require("dap").step_into() end)
   map("n", "<leader>dl", function() require("dap").run_last() end)
+
+  local dap = require("dap")
+
+          dap.configurations.scala = {
+            {
+              type = "scala",
+              request = "launch",
+              name = "RunOrTest",
+              metals = {
+                runType = "runOrTestFile",
+                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+              },
+            },
+            {
+              type = "scala",
+              request = "launch",
+              name = "Test Target",
+              metals = {
+                runType = "testTarget",
+              },
+            },
+          }
 end
 
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
