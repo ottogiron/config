64b824f631b0086003351550629805218df382b9
Otto Giron <ottog2486@gmail.com>
2024-03-08 02:09:33 +0100

    Format nicely

--- init.lua
+++ init.lua
@@ -1,12 +1,10 @@
--- disable netrw at the very start of your init.lua
+-- Disable netrw and enable 24-bit color support
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
 vim.opt.termguicolors = true
+vim.opt.swapfile = false -- Disable swap files
 
-
--- Setting up global variables and utilities
+-- Global variables and utilities
 local map = vim.keymap.set
 local fn = vim.fn
 local cmd = vim.cmd
@@ -16,10 +14,11 @@ local g = vim.g
 g.mapleader = ' '
 g.maplocalleader = ' '
 
+-- Line numbers and relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Lazy.nvim setup
+-- Lazy.nvim plugin manager setup
 local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   fn.system({
@@ -35,284 +34,123 @@ vim.opt.runtimepath:append(lazypath)
 
 -- Plugin setup
 require("lazy").setup({
-  {
-      'rose-pine/neovim',
-      name = 'rose-pine'
-  },
-  {
-      'mrcjkb/rustaceanvim',
-      version = '^4',
-      ft = { 'rust' }
-  },
-  {
-      'christoomey/vim-tmux-navigator'
-  },
-  {
-      'nvim-lua/popup.nvim'
-  },
-  {
-      'nvim-lua/plenary.nvim'
-  },
-  {
-      'nvim-telescope/telescope.nvim',
-      dependencies = {
-          'nvim-lua/popup.nvim',
-          'nvim-lua/plenary.nvim'
-      }
-  },
-  {
-      'hrsh7th/nvim-cmp',
-      event = "InsertEnter",
-      dependencies = {
-          'hrsh7th/cmp-nvim-lsp',
-          'hrsh7th/cmp-vsnip',
-          'hrsh7th/vim-vsnip'
-      },
-      opts = function()
-     	local cmp = require("cmp")
-      local conf = {
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "vsnip" },
-        },
-        snippet = {
-          expand = function(args)
-            -- Comes from vsnip
-            fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          -- None of this made sense to me when first looking into this since there
-          -- is no vim docs, but you can't have select = true here _unless_ you are
-          -- also using the snippet stuff. So keep in mind that if you remove
-          -- snippets you need to remove this select
-          ["<CR>"] = cmp.mapping.confirm({ select = true })
-        })
-      }
-      return conf
-      end
-  },
-  {
-      'kyazdani42/nvim-tree.lua',
-      dependencies = {
-          'kyazdani42/nvim-web-devicons' -- for file icons
-      }
-  },
-  {
-      'neovim/nvim-lspconfig' -- LSP Configuration
-  },
- {
-  "scalameta/nvim-metals",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    {
-	"j-hui/fidget.nvim",
-	opts = {},
-    },
-    {
-        "mfussenegger/nvim-dap",
-        config = function(self, opts)
-          -- Debug settings if you're using nvim-dap
-          local dap = require("dap")
+  -- Colorscheme
+  { 'rose-pine/neovim', name = 'rose-pine' },
 
-          dap.configurations.scala = {
-            {
-              type = "scala",
-              request = "launch",
-              name = "RunOrTest",
-              metals = {
-                runType = "runOrTestFile",
-                --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-              },
-            },
-            {
-              type = "scala",
-              request = "launch",
-              name = "Test Target",
-              metals = {
-                runType = "testTarget",
-              },
-            },
-          }
-        end
-      }
-  },
-  ft = { "scala", "sbt", "java" },
-  opts = function()
-    local metals_config = require("metals").bare_config()
+  -- Language support
+  { 'mrcjkb/rustaceanvim', version = '^4', ft = { 'rust' } },
+  { 'scalameta/nvim-metals', ft = { "scala", "sbt", "java" } },
 
-    metals_config.settings = {
-        showImplicitArguments = true,
-        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-      }
-
-    metals_config.on_attach = function(client, bufnr)
-      -- your on_attach function
-      require("metals").setup_dap()
-
-        -- LSP mappings
-        map("n", "gD", vim.lsp.buf.definition)
-        map("n", "K", vim.lsp.buf.hover)
-        map("n", "gi", vim.lsp.buf.implementation)
-        map("n", "gr", vim.lsp.buf.references)
-        map("n", "gds", vim.lsp.buf.document_symbol)
-        map("n", "gws", vim.lsp.buf.workspace_symbol)
-        map("n", "<leader>cl", vim.lsp.codelens.run)
-        map("n", "<leader>sh", vim.lsp.buf.signature_help)
-        map("n", "<leader>rn", vim.lsp.buf.rename)
-        map("n", "<leader>f", vim.lsp.buf.format)
-        map("n", "<leader>ca", vim.lsp.buf.code_action)
-
-        map("n", "<leader>ws", function()
-          require("metals").hover_worksheet()
-        end)
-
-        -- all workspace diagnostics
-        map("n", "<leader>aa", vim.diagnostic.setqflist)
-
-        -- all workspace errors
-        map("n", "<leader>ae", function()
-          vim.diagnostic.setqflist({ severity = "E" })
-        end)
-
-        -- all workspace warnings
-        map("n", "<leader>aw", function()
-          vim.diagnostic.setqflist({ severity = "W" })
-        end)
-
-        -- buffer diagnostics only
-        map("n", "<leader>d", vim.diagnostic.setloclist)
-
-        map("n", "[c", function()
-          vim.diagnostic.goto_prev({ wrap = false })
-        end)
-
-        map("n", "]c", function()
-          vim.diagnostic.goto_next({ wrap = false })
-        end)
-
-        -- Example mappings for usage with nvim-dap. If you don't use that, you can
-        -- skip these
-        map("n", "<leader>dc", function()
-          require("dap").continue()
-        end)
-
-        map("n", "<leader>dr", function()
-          require("dap").repl.toggle()
-        end)
-
-        map("n", "<leader>dK", function()
-          require("dap.ui.widgets").hover()
-        end)
-
-        map("n", "<leader>dt", function()
-          require("dap").toggle_breakpoint()
-        end)
-
-        map("n", "<leader>dso", function()
-          require("dap").step_over()
-        end)
-
-        map("n", "<leader>dsi", function()
-          require("dap").step_into()
-        end)
-
-        map("n", "<leader>dl", function()
-          require("dap").run_last()
-        end)
-    end
-
-    return metals_config
-  end,
-  config = function(self, metals_config)
-    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = self.ft,
-      callback = function()
-        require("metals").initialize_or_attach(metals_config)
-      end,
-      group = nvim_metals_group,
-    })
-  end
-  },
-  {
-      'williamboman/nvim-lsp-installer' -- Simplified LSP installer
-  },
-  {
-      'mfussenegger/nvim-dap' -- Debugging (DAP)
-  },
-  {
-      'lewis6991/gitsigns.nvim' -- Git integration
-  },
+  -- Utilities
+  { 'christoomey/vim-tmux-navigator' },
+  { 'nvim-lua/popup.nvim' },
+  { 'nvim-lua/plenary.nvim' },
+  { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' } },
+  { 'hrsh7th/nvim-cmp', event = "InsertEnter", dependencies = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-vsnip', 'hrsh7th/vim-vsnip' } },
+  { 'kyazdani42/nvim-tree.lua', dependencies = { 'kyazdani42/nvim-web-devicons' } },
+  { 'neovim/nvim-lspconfig' },
+  { 'williamboman/nvim-lsp-installer' },
+  { 'mfussenegger/nvim-dap' },
+  { 'lewis6991/gitsigns.nvim' },
 })
 
--- Nvim-tree setup
-require'nvim-tree'.setup {
-}
-
--- Telescope setup
-require('telescope').setup{
+-- Plugin configurations
+require('nvim-tree').setup({}) -- File explorer
+require('telescope').setup({ -- Fuzzy finder
   defaults = {
-    mappings = {
-      i = {
-        ["<esc>"] = require('telescope.actions').close
-      },
-    },
-    file_ignore_patterns = {"%.class$", "%.log$", "target/", "bloop/", "metals/", "project/project/", "project/target/"}
+    mappings = { i = { ["<esc>"] = require('telescope.actions').close } },
+    file_ignore_patterns = { "%.class$", "%.log$", "target/", "bloop/", "metals/", "project/project/", "project/target/" }
   },
-}
+})
+require('gitsigns').setup({}) -- Git integration
 
--- Gitsigns setup
-require('gitsigns').setup {}
-
--- LSP settings (example for Rust, extend as needed)
-require('nvim-lsp-installer').setup {}
-require'lspconfig'.rust_analyzer.setup {}
-
--- Nvim-cmp setup for autocompletion
+-- Autocompletion setup
 local cmp = require'cmp'
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({ select = true })
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-  })
+  snippet = { expand = function(args) fn["vsnip#anonymous"](args.body) end },
+  mapping = cmp.mapping.preset.insert({ ['<CR>'] = cmp.mapping.confirm({ select = true }) }),
+  sources = cmp.config.sources({ { name = 'nvim_lsp' }, { name = 'vsnip' } })
+})
+
+-- LSP setup
+require('nvim-lsp-installer').setup({})
+require'lspconfig'.rust_analyzer.setup({}) -- Rust LSP
+
+-- Metals (Scala LSP) setup
+local metals_config = require("metals").bare_config()
+metals_config.settings = {
+  showImplicitArguments = true,
+  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+}
+
+metals_config.on_attach = function(client, bufnr)
+  require("metals").setup_dap() -- DAP integration
+
+  -- LSP mappings
+  map("n", "gD", vim.lsp.buf.definition)
+  map("n", "K", vim.lsp.buf.hover)
+  map("n", "gi", vim.lsp.buf.implementation)
+  map("n", "gr", vim.lsp.buf.references)
+  map("n", "gds", vim.lsp.buf.document_symbol)
+  map("n", "gws", vim.lsp.buf.workspace_symbol)
+  map("n", "<leader>cl", vim.lsp.codelens.run)
+  map("n", "<leader>sh", vim.lsp.buf.signature_help)
+  map("n", "<leader>rn", vim.lsp.buf.rename)
+  map("n", "<leader>f", vim.lsp.buf.format)
+  map("n", "<leader>ca", vim.lsp.buf.code_action)
+  map("n", "<leader>ws", function() require("metals").hover_worksheet() end)
+
+  -- Diagnostics mappings
+  map("n", "<leader>aa", vim.diagnostic.setqflist) -- All workspace diagnostics
+  map("n", "<leader>ae", function() vim.diagnostic.setqflist({ severity = "E" }) end) -- All workspace errors
+  map("n", "<leader>aw", function() vim.diagnostic.setqflist({ severity = "W" }) end) -- All workspace warnings
+  map("n", "<leader>d", vim.diagnostic.setloclist) -- Buffer diagnostics
+  map("n", "[c", function() vim.diagnostic.goto_prev({ wrap = false }) end)
+  map("n", "]c", function() vim.diagnostic.goto_next({ wrap = false }) end)
+
+  -- DAP mappings
+  map("n", "<leader>dc", function() require("dap").continue() end)
+  map("n", "<leader>dr", function() require("dap").repl.toggle() end)
+  map("n", "<leader>dK", function() require("dap.ui.widgets").hover() end)
+  map("n", "<leader>dt", function() require("dap").toggle_breakpoint() end)
+  map("n", "<leader>dso", function() require("dap").step_over() end)
+  map("n", "<leader>dsi", function() require("dap").step_into() end)
+  map("n", "<leader>dl", function() require("dap").run_last() end)
+end
+
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt", "java" },
+  callback = function() require("metals").initialize_or_attach(metals_config) end,
+  group = nvim_metals_group,
 })
 
 -- Custom keybindings
--- Map 'jj' to exit insert mode
-vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
-map("n", "<leader>ff", ":Telescope find_files<cr>", {silent = true})
-map("n", "<leader>fg", ":Telescope live_grep<cr>", {silent = true})
-map("n", "<leader>fb", ":Telescope buffers<cr>", {silent = true})
-map("n", "<leader>fh", ":Telescope help_tags<cr>", {silent = true})
-map("n", "<leader>n", ":NvimTreeToggle<CR>", {silent = true})
-map("n", "<leader>f", ":NvimTreeFindFile<CR>", {silent = true})
+-- Exit insert mode
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true, silent = true })
 
+-- Telescope keybindings
+map("n", "<leader>ff", ":Telescope find_files<cr>", { silent = true })
+map("n", "<leader>fg", ":Telescope live_grep<cr>", { silent = true })
+map("n", "<leader>fb", ":Telescope buffers<cr>", { silent = true })
+map("n", "<leader>fh", ":Telescope help_tags<cr>", { silent = true })
 
+-- Nvim-tree keybindings
+map("n", "<leader>n", ":NvimTreeToggle<CR>", { silent = true })
+map("n", "<leader>f", ":NvimTreeFindFile<CR>", { silent = true })
+
+-- Clipboard support
 vim.opt.clipboard = "unnamedplus"
 
+-- Save and quit
 local opts = { noremap = true, silent = true }
+map('n', '<leader>w', ':w<CR>', opts) -- Save current buffer
+map('n', '<leader>q', ':q<CR>', opts) -- Quit Neovim
 
--- Save the current buffer with "leader + w"
-map('n', '<leader>w', ':w<CR>', opts)
+-- Disable search highlighting on entering insert mode
+vim.api.nvim_create_autocmd("InsertEnter", { pattern = "*", command = "set nohlsearch" })
 
--- Quit Neovim with "leader + q"
-map('n', '<leader>q', ':q<CR>', opts)
-
-vim.api.nvim_create_autocmd("InsertEnter", {
-  pattern = "*",
-  command = "set nohlsearch"
-})
-
--- Disable Arrow Keys in Normal and Visual Mode
+-- Disable arrow keys in normal and visual mode
 map('n', '<Up>', '<NOP>', opts)
 map('n', '<Down>', '<NOP>', opts)
 map('n', '<Left>', '<NOP>', opts)
@@ -322,30 +160,17 @@ map('v', '<Down>', '<NOP>', opts)
 map('v', '<Left>', '<NOP>', opts)
 map('v', '<Right>', '<NOP>', opts)
 
--- Faster Window Navigation
+-- Faster window navigation
 map('n', '<leader>h', '<C-w>h', opts)
 map('n', '<leader>j', '<C-w>j', opts)
 map('n', '<leader>k', '<C-w>k', opts)
 map('n', '<leader>l', '<C-w>l', opts)
 
-
 -- Quick access to terminal
 map('n', '<leader>t', ':split | terminal<CR>', opts)
 
--- Toggle line numbering style with "leader + ln"
+-- Toggle line numbering style
 map('n', '<leader>ln', ':set invrelativenumber<CR>', opts)
-
-
-
--- Configure <leader> keybindings for common LSP functions
-map("n", "<leader>ld", ":lua vim.lsp.buf.definition()<CR>", {silent = true})
-map("n", "<leader>lh", ":lua vim.lsp.buf.hover()<CR>", {silent = true})
-map("n", "<leader>lr", ":lua vim.lsp.buf.rename()<CR>", {silent = true})
-map("n", "<leader>la", ":lua vim.lsp.buf.code_action()<CR>", {silent = true})
 
 -- Colorscheme
 cmd("colorscheme rose-pine")
-
-
--- Nvim tree
-require("nvim-tree").setup()
