38c05b9e74adf04585e6011a0e190b55722e645a
Otto Giron <ottog2486@gmail.com>
2024-03-08 02:51:42 +0100

    Fix tmux navigator config

--- init.lua
+++ init.lua
@@ -42,7 +42,22 @@ require("lazy").setup({
   { 'scalameta/nvim-metals', ft = { "scala", "sbt", "java" } },
 
   -- Utilities
-  { 'christoomey/vim-tmux-navigator' },
+  { 'christoomey/vim-tmux-navigator',
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+  },
   { 'nvim-lua/popup.nvim' },
   { 'nvim-lua/plenary.nvim' },
   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' } },
