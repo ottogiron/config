8613dca8d2d339b2e4d35f78cb9647a0d628ee9c
ottogiron <otto.giron@gmail.com>
2026-02-18 16:43:41 +0100

    Remove unecessary backup

--- dot_config/nvim.bak.20260218/lua/custom/plugins/nvim-autopairs.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'windwp/nvim-autopairs', -- Plugin repository
-  config = function()
-    require('nvim-autopairs').setup {}
-  end,
-}
--- dot_config/nvim.bak.20260218/lua/custom/plugins/toggleterm.nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'akinsho/toggleterm.nvim',
-  version = '*',
-  keys = {
-    { '<leader>t', '<cmd>ToggleTerm<cr>', desc = '[T]erminal' },
-    { '<C-\\>', '<cmd>ToggleTerm direction=vertical<cr>', desc = 'Vertical terminal' },
-  },
-  opts = {},
-}
