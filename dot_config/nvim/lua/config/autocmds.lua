349c65226b4d8400803fd724d7c45501cc3beb96
Otto Giron <ottog2486@gmail.com>
2026-02-21 01:56:49 +0100

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Register Aster files
+vim.filetype.add {
+  extension = {
+    aster = 'aster',
+  },
+}
